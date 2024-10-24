5c4d7d676f243148c30a08676db9e0025b55eccd
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2024-10-24 12:47:59 +0200

    Add links

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,30 @@
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 import profileimage from './../img/profile.png';
 
 function About() {
   return (
-      <div className="about-container">
-          <h3>ABOUT</h3>
-          <div className="about-item">
-              <p>👋 Hi! I'm Sayaka, a passionate fullstack developer based in Stockholm, originally from Japan. I recently graduated from Linnaeus University 🎓 with a degree in Computer Science. With a background as an optician👓 and a mother of three, I bring a unique perspective to my work. In my free time, I enjoy exercising, cooking, and traveling ✈️!</p>
-              <img
-              className="about-photo"
-              src={profileimage}
-              alt="Profile"
-              />
-          </div>
+    <div className="about-container">
+    <h3>ABOUT</h3>
+      <div className="about-item">
+        <div className="about-text">
+          <p>👋 Hi! I'm Sayaka, a passionate fullstack developer based in Stockholm, originally from Japan. I recently graduated from Linnaeus University 🎓 with a degree in Computer Science. With a background as an optician👓 and a mother of three, I bring a unique perspective to my work. In my free time, I enjoy exercising, cooking, and traveling ✈️!</p>
+        </div>
+        <img
+          className="about-photo"
+          src={profileimage}
+          alt="Profile"
+        />
       </div>
+      <div className="icons-contact">
+            <a href="https://www.linkedin.com/in/sayaka-chishiki-jakobsson-315830291/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="linkedin-icon" />
+            </a>
+            <a href="https://github.com/sc222rb" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="github-icon" />
+            </a>
+      </div>
+  </div>
   );
 }
 
