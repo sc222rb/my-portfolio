81a1c271973f6ed2f2c08f41396bf9a0b8f766e1
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2024-10-25 12:24:08 +0200

    edit text

--- src/components/projects/Project01.js
+++ src/components/projects/Project01.js
@@ -9,8 +9,8 @@ function Project01 () {
             </div>
             <div className="project-item2">
                 <div className="project-text left-centered">
-                    <p>This HATEOAS-driven REST API in HAL format enables authenticated monitoring of mobile beehives, providing data on humidity, temperature, weight, and bee flow. Farmers can manage hives, report harvests, and receive notifications. Using a Kaggle sensor dataset stored in MongoDB, the API offers a reliable platform for tracking hive conditions and managing harvests.<br />  
-                    <b>Stack:</b> Node.js, Express, EJS, GitLab API, OAuth2, REST API, GraphQL</p>
+                    <p>This Node.js application was designed to implement an OAuth2 access delegation system for a server-side rendered web app, where GitLab serves as the service provider. The OAuth2 flow is built from scratch, without using external libraries, allowing users to securely log in with their GitLab accounts hosted on gitlab.lnu.se.<br />  
+                    Stack: Node.js, Express, EJS, GitLab API, OAuth2, REST API, GraphQL</p>
                 </div>
                 <div className="project-link flex-row">
                     <a href="https://github.com/sc222rb/oauth" target="_blank" rel="noopener noreferrer">
