8fee2affd707ef9cc64856e3573a58e2e1d70e64
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2024-11-08 01:31:18 +0100

    Update Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,27 @@
 import arrowImg from './../img/down-arrow.png';
+import { Player } from '@lottiefiles/react-lottie-player'
 
 function Home() {
   return ( 
     <div className='Home'>
         <div className="home-item position">
-            <h2>HI! I'M SAYAKA</h2>
-            <h6>Fullstack Developer with a background in healthcare</h6>
+          <Player 
+            src="https://lottie.host/68805721-24af-40e4-8a4f-d7c727e8da2e/M40XIVzA4Y.json" 
+            style={{ width: '300px', height: '300px' }}
+            loop
+            autoplay
+            speed={1}/>
+          <h2>I'M SAYAKA</h2>
+          <h6>Fullstack Developer with a background in healthcare</h6>
         </div>
         <div className="arrow-container">
-          <img src={arrowImg} alt="Animated arrow" className="arrow" />
+          <Player 
+            src="https://lottie.host/3be1250f-e7dc-4106-a20a-c03b34d32621/81KXV2KgBF.json" 
+            style={{ width: '100px', height: '100px' }}
+            loop
+            autoplay
+            speed={1}
+          />
         </div>
     </div>
   );
