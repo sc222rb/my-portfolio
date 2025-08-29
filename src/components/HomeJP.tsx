5050997a1c8460fe3190e403568c9e5a972bcdb5
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2025-08-29 10:57:56 +0200

    Removed the Player

--- src/components/HomeJP.tsx
+++ src/components/HomeJP.tsx
@@ -1,26 +1,10 @@
-import { Player } from '@lottiefiles/react-lottie-player'
 
 const HomeJP: React.FC = () => {
   return ( 
     <div className='Home'>
         <div className="home-item position">
-          <Player 
-            src="https://lottie.host/68805721-24af-40e4-8a4f-d7c727e8da2e/M40XIVzA4Y.json" 
-            style={{ width: '300px', height: '300px' }}
-            loop
-            autoplay
-            speed={1}/>
           <h2>I'M SAYAKA</h2>
           <h2>フルスタックエンジニア</h2>
-        </div>
-        <div className="arrow-container">
-          <Player 
-            src="https://lottie.host/3be1250f-e7dc-4106-a20a-c03b34d32621/81KXV2KgBF.json" 
-            style={{ width: '100px', height: '100px' }}
-            loop
-            autoplay
-            speed={1}
-          />
         </div>
     </div>
   );
