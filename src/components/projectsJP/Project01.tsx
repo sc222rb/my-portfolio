9554d05da351cd8d832ce31ae9fb45961f9bc2cc
Sayaka Chishiki Jakobsson <sc222rb@student.lnu.se>
2025-10-18 12:44:03 +0200

    Update Japanese descriptions

--- src/components/projectsJP/Project01.tsx
+++ src/components/projectsJP/Project01.tsx
@@ -8,7 +8,7 @@ const Project01: React.FC = () => {
             </div>
             <div className="project-item2">
                 <div className="project-text">
-                    <p>このプロジェクトは、サーバーサイドレンダリングを使用したWebアプリケーションで、三者間OAuth2フローを実装しています。これにより、ユーザーはGitLabアカウントでログインでき、GitLabプロフィールからさまざまな情報にアクセスできるようになります。OAuth2フローは外部ライブラリを使用せずに一から構築しており、ユーザーがGitLabアカウントで安全にログインできるようになっています。<br />  
+                    <p>このプロジェクトは、サーバーサイドレンダリングを使用したWebアプリケーションで、三者間OAuth2フローを独自に実装しています。ユーザーはGitLabアカウントで安全にログインでき、Expressのセッション管理を用いてアクセストークンやユーザー情報を安全に保持します。また、GitLabのREST APIを利用してユーザーのプロフィール情報や最新活動を取得・表示したり、GraphQL APIで所属グループ内のプロジェクト情報や最新コミット情報を取得・表示できるようにしました。コードはモジュール化されており、保守性と拡張性があります。このプロジェクトを通じて、OAuth2の仕組みやRESTおよびGraphQL APIを用いたデータ取得・処理の技術を習得しました。<br />  
                     <b>Stack:</b> Node.js, Express, EJS, GitLab API, OAuth2, REST API, GraphQL</p>
                 </div>
                 <div className="project-link flex-row">
